f0cce6e81db285af0a2949f4c2260f4296add122
Arthur Tetard <arthurtetard@MacBook-Air-de-Arthur.local>
2026-04-03 00:37:29 +0200

    seo

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { r as renderers } from './chunks/_@astro-renderers_B7eOfWy9.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_B4-lpxh3.mjs';
-import { manifest } from './manifest_NKQqwDwC.mjs';
+import { manifest } from './manifest_BFdsyCqt.mjs';
 
 const serverIslandMap = new Map();;
 
