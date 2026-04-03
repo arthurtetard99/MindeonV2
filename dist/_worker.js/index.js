6d1cc1a8617116c25f05bf37c2b71e476dae2423
Arthur Tetard <arthurtetard@MacBook-Air-de-Arthur.local>
2026-04-03 13:40:23 +0200

    ajout sommaire dynamique

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { r as renderers } from './chunks/_@astro-renderers_B7eOfWy9.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_B4-lpxh3.mjs';
-import { manifest } from './manifest_DB2mY8tE.mjs';
+import { manifest } from './manifest_8Rt2b6hF.mjs';
 
 const serverIslandMap = new Map();;
 
