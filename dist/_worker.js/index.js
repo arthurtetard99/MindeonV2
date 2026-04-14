9667e3180a2add869d11820681146788d0da208d
Arthur Tetard <arthurtetard@MacBook-Air-de-Arthur.local>
2026-04-14 16:45:11 +0200

    modifs page tarif

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { r as renderers } from './chunks/_@astro-renderers_B7eOfWy9.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_B4-lpxh3.mjs';
-import { manifest } from './manifest_BpF-JDoy.mjs';
+import { manifest } from './manifest_CMgI-fL1.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -13,17 +13,18 @@ const _page4 = () => import('./pages/cgu.astro.mjs');
 const _page5 = () => import('./pages/cgv.astro.mjs');
 const _page6 = () => import('./pages/contact.astro.mjs');
 const _page7 = () => import('./pages/equipe.astro.mjs');
-const _page8 = () => import('./pages/guide-vapi.astro.mjs');
-const _page9 = () => import('./pages/guides.astro.mjs');
-const _page10 = () => import('./pages/keystatic/_---params_.astro.mjs');
-const _page11 = () => import('./pages/mentions-legales.astro.mjs');
-const _page12 = () => import('./pages/nos-projets.astro.mjs');
-const _page13 = () => import('./pages/politique-de-confidentialite.astro.mjs');
-const _page14 = () => import('./pages/politique-de-cookies.astro.mjs');
-const _page15 = () => import('./pages/ressources.astro.mjs');
-const _page16 = () => import('./pages/tarifs.astro.mjs');
-const _page17 = () => import('./pages/index.astro.mjs');
-const _page18 = () => import('./pages/_---slug_.astro.mjs');
+const _page8 = () => import('./pages/guide-n8n-rag-elevenlabs.astro.mjs');
+const _page9 = () => import('./pages/guide-vapi.astro.mjs');
+const _page10 = () => import('./pages/guides.astro.mjs');
+const _page11 = () => import('./pages/keystatic/_---params_.astro.mjs');
+const _page12 = () => import('./pages/mentions-legales.astro.mjs');
+const _page13 = () => import('./pages/nos-projets.astro.mjs');
+const _page14 = () => import('./pages/politique-de-confidentialite.astro.mjs');
+const _page15 = () => import('./pages/politique-de-cookies.astro.mjs');
+const _page16 = () => import('./pages/ressources.astro.mjs');
+const _page17 = () => import('./pages/tarifs.astro.mjs');
+const _page18 = () => import('./pages/index.astro.mjs');
+const _page19 = () => import('./pages/_---slug_.astro.mjs');
 const pageMap = new Map([
     ["node_modules/@astrojs/cloudflare/dist/entrypoints/image-endpoint.js", _page0],
     ["node_modules/@keystatic/astro/internal/keystatic-api.js", _page1],
@@ -33,17 +34,18 @@ const pageMap = new Map([
     ["src/pages/cgv.astro", _page5],
     ["src/pages/contact.astro", _page6],
     ["src/pages/equipe.astro", _page7],
-    ["src/pages/guide-vapi.astro", _page8],
-    ["src/pages/guides.astro", _page9],
-    ["node_modules/@keystatic/astro/internal/keystatic-astro-page.astro", _page10],
-    ["src/pages/mentions-legales.astro", _page11],
-    ["src/pages/nos-projets.astro", _page12],
-    ["src/pages/politique-de-confidentialite.astro", _page13],
-    ["src/pages/politique-de-cookies.astro", _page14],
-    ["src/pages/ressources.astro", _page15],
-    ["src/pages/tarifs.astro", _page16],
-    ["src/pages/index.astro", _page17],
-    ["src/pages/[...slug].astro", _page18]
+    ["src/pages/guide-n8n-rag-elevenlabs.astro", _page8],
+    ["src/pages/guide-vapi.astro", _page9],
+    ["src/pages/guides.astro", _page10],
+    ["node_modules/@keystatic/astro/internal/keystatic-astro-page.astro", _page11],
+    ["src/pages/mentions-legales.astro", _page12],
+    ["src/pages/nos-projets.astro", _page13],
+    ["src/pages/politique-de-confidentialite.astro", _page14],
+    ["src/pages/politique-de-cookies.astro", _page15],
+    ["src/pages/ressources.astro", _page16],
+    ["src/pages/tarifs.astro", _page17],
+    ["src/pages/index.astro", _page18],
+    ["src/pages/[...slug].astro", _page19]
 ]);
 
 const _manifest = Object.assign(manifest, {
