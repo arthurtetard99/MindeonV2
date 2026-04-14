9c11d8b43ec9d2d7f0d42caa5a16cb70eb0b6410
Arthur Tetard <arthurtetard@MacBook-Air-de-Arthur.local>
2026-04-14 19:25:31 +0200

    modifs page tarif

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { r as renderers } from './chunks/_@astro-renderers_B7eOfWy9.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_B4-lpxh3.mjs';
-import { manifest } from './manifest_CMgI-fL1.mjs';
+import { manifest } from './manifest_BSm4vtZH.mjs';
 
 const serverIslandMap = new Map();;
 
